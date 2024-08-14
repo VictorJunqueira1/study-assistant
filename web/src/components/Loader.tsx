9aef42fe0933950a31a158e818fca23322e8afda
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-08-13 22:46:07 -0300

    [feat] Addition of a loading animation to the login screen.

--- web/src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import ClipLoader from 'react-spinners/ClipLoader';
-
-const Loader = () => (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-75 z-50">
-        <ClipLoader color="#ffffff" size={50} />
-    </div>
-);
-
-export default Loader;
