d9398fb2846cee605626527a3d5f905d7586bca7
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-11-15 01:27:11 -0300

    [uptade] Atualizando rotina.

--- web/src/types/data/data.routine.ts
+++ web/src/types/data/data.routine.ts
@@ -6,41 +6,31 @@ export const days = {
         "15h00 - 22h00: Tempo com a namorada"
     ],
     "Segunda-feira": [
-        "07h30 - 08h30: Academia",
-        "08h30 - 10h30: Programação",
-        "10h30 - 11h30: Inglês",
-        "13h00 - 18h20: Escola",
-        "19h30 - 20h00: Matemática",
-        "20h30 - 22h30: Tempo livre"
+        "08h00 - 09h00: Academia",
+        "09h30 - 11h00: Estudar Programação",
+        "13h00 - 19h00: Escola",
+        "19h30 - 20h30: Estudar Inglês",
+        "20h30 - 22h30: Tempo Livre"
     ],
     "Terça-feira": [
-        "08h30 - 10h00: Matemática",
-        "10h00 - 11h00: Inglês",
-        "11h00 - 11h30: Tempo livre",
-        "13h00 - 18h20: Escola",
-        "20h30 - 22h30: Tempo livre"
+        "Dia livre"
     ],
     "Quarta-feira": [
-        "07h30 - 08h30: Academia",
-        "08h30 - 10h30: Programação",
-        "10h30 - 11h30: Inglês",
-        "13h00 - 18h20: Escola",
-        "19h00 - 21h00: Jogar"
+        "08h00 - 09h00: Academia",
+        "09h30 - 11h00: Estudar Programação",
+        "13h00 - 19h00: Escola",
+        "19h30 - 20h30: Estudar Inglês",
+        "20h30 - 22h30: Tempo Livre"
     ],
     "Quinta-feira": [
-        "08h30 - 10h00: Matemática",
-        "10h00 - 11h00: Inglês",
-        "10h30 - 11h30: Jogar",
-        "13h00 - 18h20: Escola",
-        "20h30 - 22h30: Tempo livre"
+        "08h00 - 09h00: Academia",
+        "09h30 - 11h00: Estudar Programação",
+        "13h00 - 19h00: Escola",
+        "19h30 - 20h30: Estudar Inglês",
+        "20h30 - 22h30: Tempo Livre"
     ],
     "Sexta-feira": [
-        "07h30 - 08h30: Academia",
-        "08h30 - 10h30: Programação",
-        "10h30 - 11h30: Inglês",
-        "13h00 - 18h20: Escola",
-        "19h30 - 20h00: Matemática",
-        "20h00 - 21h30: Jogar"
+        "Dia livre"
     ],
     "Sábado": [
         "Manhã: Igreja",
