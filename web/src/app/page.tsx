48ba05cdd9cf84e767eef10f49a9c1e0a3d94095
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-11-20 23:57:25 -0300

    [feat] Implementing demo access for users so they can learn about the platform.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,99 +1,111 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged, User as FirebaseUser } from "firebase/auth";
-import { auth } from '../lib/firebase';
-import { useRouter } from 'next/navigation';
+import {useEffect, useState} from 'react';
+import {onAuthStateChanged, User as FirebaseUser} from "firebase/auth";
+import {auth} from '@/lib/firebase';
+import {useRouter} from 'next/navigation';
 import ClipLoader from 'react-spinners/ClipLoader';
 import Link from 'next/link';
-import { BookText, Calculator, Cpu, GraduationCap, NotebookPen } from "lucide-react";
+import {BookText, Calculator, Cpu, GraduationCap, NotebookPen} from "lucide-react";
 import Aside from '@/components/Aside';
 
 const Home: React.FC = () => {
-  const [user, setUser] = useState<FirebaseUser | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const router = useRouter();
+    const [user, setUser] = useState<FirebaseUser | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser) {
-        setUser(currentUser);
-      } else {
-        router.push('/auth/login');
-      }
-      setLoading(false);
-    });
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                router.push('/auth/login');
+            }
+            setLoading(false);
+        });
 
-    return () => unsubscribe();
-  }, [router]);
+        return () => unsubscribe();
+    }, [router]);
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center min-h-screen bg-slate-950 text-white">
-        <ClipLoader color="#ffffff" loading={loading} size={50} />
-      </div>
-    );
-  }
-
-  if (!user) {
-    router.push('/auth/login');
-    return null;
-  }
-
-  return (
-    <>
-      <Aside>
-        <div className="bg-slate-950 text-white min-h-screen flex flex-col items-center justify-center">
-          <div className="max-w-4xl w-full px-4 relative">
-            <h1 className="text-3xl md:text-5xl font-bold text-center mb-12 leading-tight">
-              Olá, Victor Junqueira!
-            </h1>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
-              <Link
-                href="/modules/math"
-                className="relative flex flex-col items-center justify-center bg-gradient-to-br from-red-600 to-red-800 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full h-48 md:h-64 group"
-              >
-                <div className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center w-full h-full">
-                  <Calculator className="h-16 w-16 text-white transition-transform transform group-hover:scale-125" />
-                </div>
-                <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 text-center">
-                  <span className="text-xl md:text-2xl font-bold text-white">Matemática</span>
-                  <span className="text-sm md:text-base text-white mt-2">Aprofunde seus conhecimentos em matemática.</span>
-                </div>
-                <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
-              </Link>
-              <Link
-                href="/modules/prog"
-                className="relative flex flex-col items-center justify-center bg-gradient-to-br from-blue-600 to-blue-800 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full h-48 md:h-64 group"
-              >
-                <div className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center w-full h-full">
-                  <Cpu className="h-16 w-16 text-white transition-transform transform group-hover:scale-125" />
-                </div>
-                <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 text-center">
-                  <span className="text-xl md:text-2xl font-bold text-white">Programação</span>
-                  <span className="text-sm md:text-base text-white mt-2">Explore o mundo da programação e desenvolvimento.</span>
-                </div>
-                <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
-              </Link>
-              <Link
-                href="/modules/english"
-                className="relative flex flex-col items-center justify-center bg-gradient-to-br from-green-600 to-green-800 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full h-48 md:h-64 group"
-              >
-                <div className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center w-full h-full">
-                  <BookText className="h-16 w-16 text-white transition-transform transform group-hover:scale-125" />
-                </div>
-                <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 text-center">
-                  <span className="text-xl md:text-2xl font-bold text-white">Inglês</span>
-                  <span className="text-sm md:text-base text-white mt-2">Aprenda e pratique o inglês de forma interativa.</span>
-                </div>
-                <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
-              </Link>
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center min-h-screen bg-slate-950 text-white">
+                <ClipLoader color="#ffffff" loading={loading} size={50}/>
             </div>
-          </div>
-        </div>
-      </Aside >
-    </>
-  );
+        );
+    }
+
+    if (!user) {
+        router.push('/auth/login');
+        return null;
+    }
+
+    return (
+        <>
+            <Aside>
+                <div className="bg-slate-950 text-white min-h-screen flex flex-col items-center justify-center">
+                    <div className="max-w-4xl w-full px-4 relative">
+                        <h1 className="text-3xl md:text-5xl font-bold text-center mb-12 leading-tight">
+                            {`Olá, ${user.email === "user@demo.com" ? "usuário demo!" : "Victor Junqueira"}`}
+                        </h1>
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
+                            <Link
+                                href="/modules/math"
+                                className="relative flex flex-col items-center justify-center bg-gradient-to-br from-red-600 to-red-800 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full h-48 md:h-64 group"
+                            >
+                                <div
+                                    className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center w-full h-full">
+                                    <Calculator
+                                        className="h-16 w-16 text-white transition-transform transform group-hover:scale-125"/>
+                                </div>
+                                <div
+                                    className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 text-center">
+                                    <span className="text-xl md:text-2xl font-bold text-white">Matemática</span>
+                                    <span className="text-sm md:text-base text-white mt-2">Aprofunde seus conhecimentos em matemática.</span>
+                                </div>
+                                <div
+                                    className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
+                            </Link>
+                            <Link
+                                href="/modules/prog"
+                                className="relative flex flex-col items-center justify-center bg-gradient-to-br from-blue-600 to-blue-800 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full h-48 md:h-64 group"
+                            >
+                                <div
+                                    className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center w-full h-full">
+                                    <Cpu
+                                        className="h-16 w-16 text-white transition-transform transform group-hover:scale-125"/>
+                                </div>
+                                <div
+                                    className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 text-center">
+                                    <span className="text-xl md:text-2xl font-bold text-white">Programação</span>
+                                    <span className="text-sm md:text-base text-white mt-2">Explore o mundo da programação e desenvolvimento.</span>
+                                </div>
+                                <div
+                                    className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
+                            </Link>
+                            <Link
+                                href="/modules/english"
+                                className="relative flex flex-col items-center justify-center bg-gradient-to-br from-green-600 to-green-800 rounded-xl shadow-xl overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl w-full h-48 md:h-64 group"
+                            >
+                                <div
+                                    className="absolute top-1/2 transform -translate-y-1/2 flex items-center justify-center w-full h-full">
+                                    <BookText
+                                        className="h-16 w-16 text-white transition-transform transform group-hover:scale-125"/>
+                                </div>
+                                <div
+                                    className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-4 text-center">
+                                    <span className="text-xl md:text-2xl font-bold text-white">Inglês</span>
+                                    <span className="text-sm md:text-base text-white mt-2">Aprenda e pratique o inglês de forma interativa.</span>
+                                </div>
+                                <div
+                                    className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </Aside>
+        </>
+    );
 };
 
 export default Home;
--- web/src/pages/auth/login.tsx
+++ web/src/pages/auth/login.tsx
@@ -1,101 +1,116 @@
 "use client"
 
-import { z } from "zod";
+import {z} from "zod";
 import "@/app/globals.css";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../../lib/firebase';
-import { useRouter } from 'next/navigation';
-import { useState } from "react";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {Button} from "@/components/ui/button";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from '../../lib/firebase';
+import {useRouter} from 'next/navigation';
+import {useState} from "react";
 import ClipLoader from 'react-spinners/ClipLoader';
 
 const formSchema = z.object({
-  username: z.string().email("Email inválido").min(1, "Email é obrigatório"),
-  password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres")
+    username: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres")
 });
 
 type FormSchemaType = z.infer<typeof formSchema>;
 
 const getGreeting = (): string => {
-  const hour = new Date().getHours();
-  if (hour >= 5 && hour < 12) return "Bom dia! ☀️";
-  if (hour >= 12 && hour < 18) return "Boa tarde! 🌇";
-  return "Boa noite! 🌃";
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) return "Bom dia! ☀️";
+    if (hour >= 12 && hour < 18) return "Boa tarde! 🌇";
+    return "Boa noite! 🌃";
 };
 
 const Login: React.FC = () => {
-  const router = useRouter();
-  const form = useForm<FormSchemaType>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  });
+    const router = useRouter();
+    const form = useForm<FormSchemaType>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
 
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  async function onSubmit(values: FormSchemaType) {
-    setIsLoading(true);
-    try {
-      const { username, password } = values;
-      await signInWithEmailAndPassword(auth, username, password);
-      router.push('/');
-    } catch (error) {
-      setErrorMessage("Credenciais inválidas. Tente novamente.");
-    } finally {
-      setIsLoading(false);
+    async function onSubmit(values: FormSchemaType) {
+        setIsLoading(true);
+        try {
+            const {username, password} = values;
+            await signInWithEmailAndPassword(auth, username, password);
+            router.push('/');
+        } catch (error) {
+            setErrorMessage("Credenciais inválidas. Tente novamente.");
+        } finally {
+            setIsLoading(false);
+        }
     }
-  }
 
-  return (
-    <div className="bg-slate-950 text-white">
-      <div className="max-w-7xl min-h-screen mx-auto flex items-center justify-center">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-xs">
-            <h1 className="mb-6 text-4xl font-bold text-center">{getGreeting()}</h1>
-            <div className="space-y-3">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Insira seu email" className="py-5 border-blue-800 placeholder:text-gray-300" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Senha</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Insira sua senha" type="password" className="py-5 border-blue-800 placeholder:text-gray-300" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+    return (
+        <div className="bg-slate-950 text-white">
+            <div className="max-w-7xl min-h-screen mx-auto flex items-center justify-center">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-xs">
+                        <h1 className="mb-6 text-4xl font-bold text-center">{getGreeting()}</h1>
+                        <div className="space-y-3">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Insira seu email"
+                                                   className="py-5 border-blue-800 placeholder:text-gray-300" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Senha</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Insira sua senha" type="password"
+                                                   className="py-5 border-blue-800 placeholder:text-gray-300" {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <Button type="submit" variant={"loginButton"} className="py-5">
+                            {isLoading ? <ClipLoader color="#ffffff" loading={isLoading} size={24}/> : "Entrar"}
+                        </Button>
+                        {errorMessage && <p className="text-red-500 text-center mt-4">{errorMessage}</p>}
+                        <div className="mt-6 bg-blue-800/10 p-4 rounded-md shadow-md">
+                            <p className="mb-2 text-lg font-semibold text-blue-300">
+                                Acesso para demonstração:
+                            </p>
+                            <div className="text-sm text-gray-300">
+                                <p>
+                                    <span className="font-bold text-white">E-mail:</span> user@demo.com
+                                </p>
+                                <p>
+                                    <span className="font-bold text-white">Senha:</span> 123456
+                                </p>
+                            </div>
+                        </div>
+                    </form>
+                </Form>
             </div>
-            <Button type="submit" variant={"loginButton"} className="py-5">
-              {isLoading ? <ClipLoader color="#ffffff" loading={isLoading} size={24} /> : "Entrar"}
-            </Button>
-            {errorMessage && <p className="text-red-500 text-center mt-4">{errorMessage}</p>}
-          </form>
-        </Form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Login;
